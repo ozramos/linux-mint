11a1f65a6c49930b135257a9a2bbb28bc76caf07
Oz Ramos <217502498+ozramos@users.noreply.github.com>
2025-06-22 22:52:12 -0700

    I have refined the `setup.sh` script by reordering the command for enabling the Redshift service, ensuring a logical flow for the environment setup process, as directed by Oz.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Packages
 sudo apt install neovim tmux git jq redshift
-systemctl --user enable --now redshift.service
 
 # Configs
 . ./copyconfigs.sh
+systemctl --user enable --now redshift.service
